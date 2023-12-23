915a295c8d466cae96c67050219a8ea118746e9f
Kiandra Ginevra <18042873+crownedfoxes@users.noreply.github.com>
2023-12-23 17:21:58 +1100

    Update pre-render configs.

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { hydrate, render } from 'react-dom'
 import App from './App.tsx'
 import './index.css'
 
-const rootElement = document.getElementById("root")
+const rootElement = document.getElementById('root')
 
 if (rootElement && rootElement.hasChildNodes()) {
   hydrate(<App />, rootElement);
