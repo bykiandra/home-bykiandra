1b1cd74879bccf9845b8d3b986a34ab962e64a41
Kiandra Ginevra <18042873+crownedfoxes@users.noreply.github.com>
2023-12-23 17:12:05 +1100

    Adds pre-rendering.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { hydrate, render } from 'react-dom'
 import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const rootElement = document.getElementById("root")
+
+if (rootElement && rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
